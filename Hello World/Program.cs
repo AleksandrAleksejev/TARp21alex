d036812893ead496aa3f13b69045281787db1f5f
Opilane TTHK <opilane@tthk.ee>
2021-11-09 09:50:43 +0200

    ask first name

---
+++ Hello World/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Hello_World
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter your first name:") ;
+            string firstname = Console.ReadLine();
+            Console.WriteLine("Hello," + firstname + "!");
+
+
+            Console.Read();
+        }
+    }
+}
